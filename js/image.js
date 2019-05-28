35c227e6366b52765ecc6c730fcb371d932cef11
liszp <z@lisz.vip>
2019-05-28 19:59:34 +0800

    add 从剪贴板获取图片直接上传

--- js/image.js
+++ js/image.js
@@ -8,6 +8,7 @@ var image = {
         this.refreshImage();
         ID('imageList').style.display = 'block';
     },
+    // 初始化 绑定事件
     initEvent: function () {
         var that = this,
             file = ID('file');
@@ -15,7 +16,7 @@ var image = {
         ID('btn').addEventListener('click', function () {
             that.start();
         });
-
+        // 上传图片
         file.addEventListener('change', function (e) {
             var file = e.target.files[0],
                 name = cos.getRandomName(this.value);
@@ -31,6 +32,37 @@ var image = {
             }
         });
 
+        // 鼠标移上 显示大图
+        ID('imageList').addEventListener('mouseover', function (e) {
+            var target = e.target || e.srcElement;
+            if (target.tagName === 'IMG') {
+                that.showBigImage(target.getAttribute('src'))
+            }
+        });
+
+        ID('imageList').addEventListener('mouseout', function (e) {
+            var target = e.target || e.srcElement;
+            if (target.tagName === 'IMG') {
+                ID('showImage').style.display = 'none';
+            }
+        });
+
+        // 粘帖图片
+        ID('clipboardInput').addEventListener('paste', function(event) {
+            var items = (event.clipboardData && event.clipboardData.items) || [];
+            var file = null;
+
+            if (items && items.length) {
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].type.indexOf('image') !== -1) {
+                        file = items[i].getAsFile();
+                        cos.send(cos.getRandomName(file.name), file);
+                        break;
+                    }
+                }
+            }
+        });
+
         // 清除历史
         ID('clean').addEventListener('click', function () {
             chrome.storage.sync.set({image: []}, function() {
@@ -38,28 +70,28 @@ var image = {
             });
         })
     },
+    // 点击上传按钮
     start: function () {
-        var that = this,
-            file = ID('file');
-        file.click();
+        ID('file').click();
     },
-    // 显示图片
+    // 加载本地历史 显示图片
     refreshImage: function () {
         chrome.storage.sync.get({image: []}, function(items) {
             var html = [];
             var images = items.image.reverse();
             for (var i=0;i<items.image.length;i++) {
-                html.push('<li><a href="javascript:;"><img src="'+items.image[i]+'"></a></li>');
+                html.push('<li><a href="#"><img src="'+items.image[i]+'"></a></li>');
             }
             ID('imageList').innerHTML = html.join('');
         });
     },
+    // 保存到本地记录
     insert: function (src) {
         var that = this;
         chrome.storage.sync.get({image: []}, function(items) {
             var data = items.image;
             data.push(src);
-            if (data.length > 10) {
+            if (data.length > 100) {
                 data.shift();
             }
             chrome.storage.sync.set({image: data}, function() {
@@ -68,6 +100,12 @@ var image = {
             });
         });
         this.copy(src);
+    },
+    // 显示大图
+    showBigImage (src) {
+        var image = document.querySelector('#showImage img');
+        image.setAttribute('src', src);
+        ID('showImage').style.display = 'block';
     },
     // 复制文字
     copy: function (txt) {
@@ -93,6 +131,7 @@ var image = {
 };
 
 var cos = {
+    // 上传图片到腾讯云
     send: function (name, file) {
         var config = {
             SecretId: '',
@@ -125,7 +164,7 @@ var cos = {
     },
     // 获取随机文件名称
     getRandomName: function (name) {
-        var ext = name.split('.').pop(),
+        var ext = name ? name.split('.').pop() : '',
             d = new Date(),
             path = "/";
         path += d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
--- readme.md
+++ readme.md
@@ -1,10 +1,18 @@
-
-- 图片上传完成后自动加入剪贴板，保留10条历史记录
-- 点击图片复制 `markdown` 格式图片
-- 需要配置[腾讯云存储参数](https://console.cloud.tencent.com/cos)
+## `markdown`图片助手
 
 ![image](logo.png)
 
-![image](https://note-1251721932.cos.ap-shanghai.myqcloud.com/2018-7-12/fm0vr0bnk6h.png)
+- 截图后直接粘帖到文本框
+
+![image](https://note-1251721932.cos.ap-shanghai.myqcloud.com/2019-5-28/go0hk9pka7u.png)
+
+- 图片上传完成后自动加入剪贴板
+- 点击图片复制 `markdown` 格式图片
+- 需要配置[腾讯云存储参数](https://console.cloud.tencent.com/cos)
+
+| 来源 Origin | 操作 Methods | Allow-Headers | Expose-Headers | 超时 Max-Age |
+|-- | -- | -- | -- | -- |
+| * | PUT,GET,POST | * | ETag | 30 |
+
 
 ![image](https://note-1251721932.cos.ap-shanghai.myqcloud.com/2018-7-12/4fgtltq2xeg.png)
